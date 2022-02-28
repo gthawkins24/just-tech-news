3aa0b9fdd3ea7f1fb6989ed6080becb6443004cf
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-02-27 21:58:45 -0500

    started authentication

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('DB_NAME', 'DB_USER', 'DB_PW', {
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
